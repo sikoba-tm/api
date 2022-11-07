03ccdeb7a29af0115b9b9ca918fb1d60f7633307
Radhiansya Zain Antriksa Putra <radhi.rz666@gmail.com>
2022-11-07 14:57:58 +0700

    feat: add CORS configuration

--- cmd/app.go
+++ cmd/app.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/sikoba-tm/api/core/service"
 	"github.com/sikoba-tm/api/handler"
 	"github.com/sikoba-tm/api/repository"
@@ -14,6 +15,12 @@ func Run() {
 
 	port := getPort()
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 
 	bencanaRepository := repository.NewBencanaRepository(db)
 	bencanaService := service.NewBencanaService(bencanaRepository)
